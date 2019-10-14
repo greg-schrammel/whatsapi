f6e361ccfcb00e494074f780d318c64eff69abea
gregory schrammel <greg@gregs.local>
2019-10-14 13:18:39 -0300

    i

---
+++ web/pages/index/sessionKeyMachine.js
@@ -0,0 +1,79 @@
+import { Machine, assign } from 'xstate';
+
+import { connect, generateQR, haveKey } from './whatsapp_session';
+
+export default Machine(
+  {
+    initial: 'init',
+    context: {
+      connection: connect(),
+      qr: undefined,
+      key: undefined,
+    },
+    states: {
+      generatingQR: {
+        invoke: {
+          id: 'awaitQR',
+          src: 'generateQR',
+        },
+        on: {
+          QR_READY: {
+            target: 'awaitingQRScan',
+            actions: 'updateQR',
+          },
+        },
+      },
+      awaitingQRScan: {
+        invoke: {
+          id: 'haveKey',
+          src: 'haveKey',
+        },
+        on: {
+          REFRESH_QR: {
+            target: 'refreshingQR',
+            actions: 'updateQR',
+          },
+          GOT_KEY: {
+            target: 'success',
+            actions: 'setKey',
+          },
+        },
+      },
+      failed: {
+        on: {
+          RETRY: 'generatingQR',
+        },
+      },
+      success: {
+        type: 'final',
+      },
+      on: {
+        ERROR: 'error',
+      },
+    },
+  },
+  {
+    services: {
+      generateQR: (ctx, _event) => send =>
+        generateQR(
+          ctx.connection,
+          value => send({ type: 'QR_READY', value }),
+          value => send({ type: 'ERROR', value }),
+        ),
+      haveKey: (ctx, _event) => send =>
+        haveKey(
+          ctx.connection,
+          value => send({ type: 'GOT_KEY', value }),
+          value => send({ type: 'ERROR', value }),
+        ),
+    },
+    actions: {
+      setQR: assign({
+        qr: (_ctx, event) => event,
+      }),
+      setKey: assign({
+        key: (_ctx, event) => event,
+      }),
+    },
+  },
+);
